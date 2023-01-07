20db175712a8721a81ac0bd0f1ed428c1b5d4c83
AmanPachori <amangirrajpachori2002@gmail.com>
2023-01-08 00:32:48 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <TechStack />
       <About/>
       <Projects/>
-      <About/>
 
     </div>
   );
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,30 +1,53 @@
 import React from 'react'
+import {data} from "../Data/project"
 
 const Projects = () => {
-  return (
+  return(
     <div class="container project my-3">
-      <h5 className="fs-1 fw-bold m-2 p-2 py-3">Projects</h5>
-      <div className="row">
-        <div className="col-lg-6 py-3 col-sm-12 projectInfo d-flex flex-column align-items-center justify-content-center">
-          <h5 className="fs-3 fw-bolder"> Project Name</h5>
-          <p className="fs-5 fw-bold">
-            lorem isdsdsg .vsdj some redndvvd dvvnsjj
-          </p>
-          <button className="btn rounded p-2 fw-bold my-2">See Code</button>
-        </div>
-        <div className="col-lg-6 my-2 projects col-sm-12">
-          <img
-            src="https://i.picsum.photos/id/972/536/354.jpg?hmac=YUVQvWb_AFQBjRRoPhOjfPqw2kdeEgglmWAD-U39--o"
-            alt="Image"
-            className='rounded'
-          />
-          <div class="caption">
-            <h1>Project Name</h1>
+    <h5 className="fs-1 fw-bold m-2 p-2 py-3">Projects</h5>
+    <div className="row">
+      {data.map((e) => {
+        if (e.id % 2 == 0) {
+          return (
+            <>
+              <div className="col-lg-6 my-2 py-3 col-sm-12 projectInfo d-flex flex-column align-items-center justify-content-center">
+                <h5 className="fs-3 fw-bolder">{e.name}</h5>
+                <p className="fs-5 fw-bold">{e.desc}</p>
+                <a target='_blank' href={e.link} className="btn rounded p-2 fw-bold my-2">
+                  See Code
+                </a>
+              </div>
+              <div className="col-lg-6 my-2 projects col-sm-12">
+                <img src={e.img} alt="Image" className="rounded" />
+                {/* <div class="caption">
+                  <h1>{e.name}</h1>
+                </div> */}
+              </div>
+            </>
+          );
+        } else {
+         return(
+          <>
+          <div className="col-lg-6 my-2 py-3 projects col-sm-12">
+            <img src={e.img} alt="Image" className="rounded" />
+            {/* <div class="caption">
+              <h1>{e.name}</h1>
+            </div> */}
           </div>
-        </div>
-      </div>
+          <div className="col-lg-6 py-3 my-2 col-sm-12 projectInfo d-flex flex-column align-items-center justify-content-center">
+            <h5 className="fs-3 fw-bolder"> {e.name}</h5>
+            <p className="fs-5 fw-bold">{e.desc}</p>
+            <a target='_blank' href={e.link} className="btn rounded p-2 fw-bold my-2">
+              See Code
+            </a>
+          </div>
+        </>
+         )
+        }
+      })}
     </div>
-  );
+  </div>
+  )
 }
 
 export default Projects
