f14407cb9bf397689bcb91043ab254dd7b458c06
AmanPachori <amangirrajpachori2002@gmail.com>
2023-01-11 23:24:29 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TechStack from './components/Techstack/techStack'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './components/About/about';
 import Projects from './components/Projects/projects';
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <TechStack />
       <About/>
       <Projects/>
-
+      <Footer/>
     </div>
   );
 }
