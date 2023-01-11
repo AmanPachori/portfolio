cf016002a419545f85a4aa34628212e0ed412d05
AmanPachori <amangirrajpachori2002@gmail.com>
2023-01-12 00:12:08 +0530

    resume button added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,27 +9,39 @@ import Typewriter from "typewriter-effect";
 const Home = () => {
   return (
     <div className="Home">
-        <Container className="container  my-5">
+      <Container className="container  my-5">
         <Row>
-            <Col className="p-5 my-2 text-start">
+          <Col className="p-5 my-2 text-start">
             <p className="fs-1 fw-bold">Hello</p>
-            <p className="fs-3 fw-bolder"> My Name is Aman <br/> I'm a student at IIIT Gwalior and a <span className="fs-3 fw-bolder" style={{ color: "var(--primaryColor)" }}><Typewriter options={{
-                autoStart: true,
-                loop: true,
-                delay: 45,
-                strings:[
-                 "Full Stack Developer",
-                 "Open Source Contributor", 
-                 "Blockchain enthusiast"  
-                ]
-            }}>
-          </Typewriter>{" "}</span></p>
-            </Col>
-
+            <p className="fs-3 fw-bolder">
+              {" "}
+              My Name is Aman <br /> I'm a student at IIIT Gwalior and a{" "}
+              <span
+                className="fs-3 fw-bolder"
+                style={{ color: "var(--primaryColor)" }}
+              >
+                <Typewriter
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                    delay: 45,
+                    strings: [
+                      "Full Stack Developer",
+                      "Open Source Contributor",
+                      "Blockchain enthusiast",
+                    ],
+                  }}
+                ></Typewriter>{" "}
+                <a href='https://drive.google.com/file/d/17G_49OWRe8CEeNHoAUT1lf1KNpMCRGiY/view' target="_blank" >
+                <button className='border-0 resumeButton rounded p-2 fs-5 my-2'>Resume </button>
+                </a>
+              </span>
+            </p>
+          </Col>
         </Row>
-        </Container>
+      </Container>
     </div>
-  )
+  );
 }
 
 export default Home
