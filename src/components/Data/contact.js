f39cb4c4b328fe25fef62177194abf5535c2aa34
AmanPachori <amangirrajpachori2002@gmail.com>
2023-01-11 23:23:35 +0530

    conttact data added

---
+++ src/components/Data/contact.js
@@ -0,0 +1,5 @@
+export const contactData = [
+  {id:'1',name:'Github',img:'https://img.icons8.com/ios-glyphs/58/FFFFFF/github.png',link:'https://github.com/AmanPachori'},
+  {id:'2',name:'LinkedIn',img:'https://img.icons8.com/fluency/58/null/linkedin.png',link:'https://www.linkedin.com/in/aman-pachori-aa22a121a/'},
+  {id:'3',name:'Gmail',img:'https://img.icons8.com/color/58/null/gmail-new.png',link:'amangirrajpachori2002@gmail.com'}
+]
