f7e0d6feb10c384180e56f05de39d28202b144e4
AmanPachori <amangirrajpachori2002@gmail.com>
2023-01-17 22:08:46 +0530

    minor fixes

--- src/components/Footer/Icons/footerIcons.js
+++ src/components/Footer/Icons/footerIcons.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FooterIcons = ({e}) => {
   return (
-    <div className='mx-2 Icons'>
+    <div className='mx-1 d-flex align-items-center justify-content-center Icons'>
       <a href={e.link} target='_blank'>
       <img src={e.img}/>
       </a>
